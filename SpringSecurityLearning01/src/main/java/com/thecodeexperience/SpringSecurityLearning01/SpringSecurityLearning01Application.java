b39589a083ab029707f0b36b7adce2acacb01a7b
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2025-11-17 22:14:45 +0530

    spring Security Learning added

---
+++ SpringSecurityLearning01/src/main/java/com/thecodeexperience/SpringSecurityLearning01/SpringSecurityLearning01Application.java
@@ -0,0 +1,13 @@
+package com.thecodeexperience.SpringSecurityLearning01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLearning01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLearning01Application.class, args);
+	}
+
+}
